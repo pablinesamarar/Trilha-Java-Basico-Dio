a3fa5b2c1f6e0f1ad75f5ee1bcee9b15eaabf3c4
pablinesamara <pablinesamara@gmail.com>
2024-05-23 18:22:32 -0300

    tipos de variaveis alteracao e convencoes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Trilha-Java-Basico-Dio
 Repositório para projetos do curso básico de java Dio
+
+- Tipos primitivos:
+- int,byte short, long , float, double, boolean e char esses tipos não são considerados objetos, e portanto representam valores brutps.
+-Eles são armazenados diretamente na pilha de memorias.(Memory stack)
+-Tipos primitivos podem conter partes fracionarias: float, double
+- Variaveis contantes devem iniciar com final ex final String meuNome = "Pabline";
--- projeto1/src/App.java
+++ projeto1/src/App.java
@@ -1,5 +1,24 @@
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; //se comecar com 0 provalve necessidade de usar string
+        long cpf = 987654321L; //Terminar com L
+        float pi = 3.14F; //Termina com F
+        double salarioMinimo = 1333.16;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto =(short) numeroNormal;//conversao em casting pois o numero int pode nao caber dentro do short por isso pode ocorrer erros
+
+        final String CAIXAALTA = "variavel_inalteravel";// inserir o final garante que o programa nao permite alterar essa vaariavel
+
+        final double VALORDEPI = 3.13;
+        
+        
     }
 }
